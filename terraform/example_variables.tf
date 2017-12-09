cfe457f976b7e3d9dac860aeed5925b48baeb577
Chris Walsh <chris24walsh@gmail.com>
2017-12-09 13:11:54 +0100

    Add example variables.tf file
    Exchange key values with appropriate private values.

---
+++ terraform/example_variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  default = "<access_key>"
+}
+variable "secret_key" {
+  default = "<secret_key>"
+}
+variable "region" {
+  default = "eu-west-1"
+}
+variable "ami" {}
+variable "eip" {
+	default = "<eip>"
+}
+variable "key_name" {
+	default = "<key_name>"
+}
