5442abf22fcc7026154d234e48690142aeb3e904
Chris Walsh <chris24walsh@gmail.com>
2017-12-09 12:19:28 +0100

    Download phaser js file using wget class
    Add ordering to manifest, using 'before' attributes.
    Add directory to hold phaser js file.

--- main.pp
+++ main.pp
@@ -5,6 +5,7 @@ class { 'apache':
 apache::vhost { 'spacehorse.chriswalsh.tk':
   port    => '80',
   docroot => '/var/www/spacehorse',
+	before => Vcsrepo['/var/www/spacehorse'],
 }
 
 package {'git':
@@ -15,4 +16,21 @@ vcsrepo { '/var/www/spacehorse':
 	ensure => present,
 	provider => git,
 	source => 'git://github.com/chris24walsh/spacehorse2.0.git',
+	before => File['js_folder'],
+}
+
+file { 'js_folder':
+	path => '/var/www/spacehorse/js',
+	ensure => 'directory',
+	before => Wget::Fetch['phaser.min.js'],
+}
+
+include wget
+
+wget::fetch {'phaser.min.js':
+	source => 'https://github.com/photonstorm/phaser-ce/releases/download/v2.9.2/phaser.min.js',
+	destination => '/var/www/spacehorse/js/phaser.min.js',
+	timeout            => 0,
+  verbose            => true,
+  nocheckcertificate => true,
 }
